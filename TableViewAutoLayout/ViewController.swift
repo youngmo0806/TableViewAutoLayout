f7a4efc427a62a409ffb4039ba331d6fc8b2c468
youngmo <youngmo0806@naver.com>
2022-12-28 17:13:27 +0900

    Initial Commit

---
+++ TableViewAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewAutoLayout
+//
+//  Created by youngmo on 2022/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
